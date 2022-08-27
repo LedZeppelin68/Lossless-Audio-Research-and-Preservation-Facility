3e21df41117b7d44365843ac76f3663b63d0cb5f
Андрей Широчкин <LedZeppelin68@gmail.com>
2022-08-27 20:47:42 +0300

    bin2wav ver1

--- Lossless Audio Research and Preservation Facility/Form1.Designer.cs
+++ Lossless Audio Research and Preservation Facility/Form1.Designer.cs
@@ -28,13 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tableLayoutPanel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "bin 2 wav";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.36364F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 63.63636F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(786, 418);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(780, 146);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 155);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(780, 260);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+            this.dataGridView1.DragOver += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragOver);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "filename";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "file mode",
+            "folder mode"});
+            this.comboBox1.Location = new System.Drawing.Point(57, 58);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(694, 56);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(51, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(597, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Simultaneous files";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Lossless Audio Research and Preservation Facility";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
--- Lossless Audio Research and Preservation Facility/Form1.cs
+++ Lossless Audio Research and Preservation Facility/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Lossless_Audio_Research_and_Preservation_Facility
 {
@@ -15,6 +17,167 @@ namespace Lossless_Audio_Research_and_Preservation_Facility
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void dataGridView1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] allfiles = ((string[])e.Data.GetData(DataFormats.FileDrop));
+
+            dataGridView1.Rows.Clear();
+
+            int mode = comboBox1.SelectedIndex;
+
+            List<string> selectedfiles = new List<string>();
+
+            switch (mode)
+            {
+                case 0:
+                    for (int i = 0; i < allfiles.Length; i++)
+                    {
+                        try
+                        {
+                            selectedfiles.AddRange(Directory.GetFiles(allfiles[i]).Where(x => x.EndsWith(".bin") || x.EndsWith(".cue")).ToArray());
+                        }
+                        catch { };
+                    }
+                    break;
+                case 1:
+                    selectedfiles.AddRange(allfiles.Where(x => new FileInfo(x).Attributes == FileAttributes.Directory).ToArray());
+                    break;
+            }
+
+            for (int i = 0; i < selectedfiles.Count; i++)
+            {
+                dataGridView1.Rows.Add(selectedfiles[i]);
+            }
+        }
+
+        static byte[] header = new byte[] { 0x52, 0x49, 0x46, 0x46, 0xFF, 0xFF, 0xFF, 0xFF, 0x57, 0x41, 0x56, 0x45, 0x66, 0x6D, 0x74, 0x20, 0x10, 0x00, 0x00, 0x00, 0x01, 0x00, 0x02, 0x00, 0x44, 0xAC, 0x00, 0x00, 0x10, 0xB1, 0x02, 0x00, 0x04, 0x00, 0x10, 0x00, 0x64, 0x61, 0x74, 0x61, 0xFF, 0xFF, 0xFF, 0xFF };
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int maxfiles = int.Parse(numericUpDown1.Value.ToString());
+
+            Parallel.For(0, dataGridView1.Rows.Count, new ParallelOptions { MaxDegreeOfParallelism = maxfiles }, i =>
+            {
+                Invoke((MethodInvoker)delegate
+                {
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Yellow;
+                });
+
+                string file = dataGridView1.Rows[i].Cells[0].Value.ToString();
+
+                switch (new DirectoryInfo(file).Attributes == FileAttributes.Directory)
+                {
+                    case false:
+                        string ext = file.Substring(file.Length - 3);
+
+                        switch (ext)
+                        {
+                            case "bin":
+                                using (BinaryReader br = new BinaryReader(new FileStream(file, FileMode.Open, FileAccess.Read)))
+                                {
+                                    using (BinaryWriter bw = new BinaryWriter(new FileStream(file.Replace(".bin", ".wav"), FileMode.Create)))
+                                    {
+                                        bw.Write(header);
+                                        br.BaseStream.CopyTo(bw.BaseStream);
+
+                                        bw.BaseStream.Position = 4;
+                                        bw.Write((uint)br.BaseStream.Length + 36);
+
+                                        bw.BaseStream.Position = 40;
+                                        bw.Write((uint)br.BaseStream.Length);
+                                    }
+                                }
+                                break;
+                            case "cue":
+                                string cue_text = File.ReadAllText(file);
+                                cue_text = cue_text.Replace("BINARY", "AUDIO");
+                                File.WriteAllText(file + ".(new).cue", cue_text);
+                                break;
+                        }
+                        break;
+                    case true:
+                        string[] bins = Directory.GetFiles(file, "*.bin");
+                        
+                        string originalcue = Directory.GetFiles(file, "*.cue").First();
+
+                        string[] indexes = File.ReadAllLines(originalcue).Where(x => x.Contains("INDEX 01")).Select(x => Regex.Match(x, "[0-9]{2}:[0-9]{2}:[0-9]{2}").Value ).ToArray();
+
+                        string newfilename = Path.GetFileName(file) + ".wav";
+                        string newcuename = Path.GetFileName(file) + ".full.cue";
+
+                        List<string> cuesheet = new List<string>();
+                        cuesheet.Add($"FILE \"{newfilename}\" AUDIO");
+
+                        int leadout = 0;
+
+                        using (BinaryWriter bw = new BinaryWriter(new FileStream(Path.Combine(file, newfilename), FileMode.Create)))
+                        {
+                            bw.Write(header);
+
+                            for (int j = 0; j < bins.Length; j++)
+                            {
+                                using (BinaryReader br = new BinaryReader(new FileStream(bins[j], FileMode.Open)))
+                                {
+                                    cuesheet.Add($"  TRACK {(j + 1):D2} AUDIO");
+                                    cuesheet.Add($"    INDEX 01 {int2msf(leadout, indexes[j])}");
+
+                                    leadout += (int)br.BaseStream.Length;
+
+                                    br.BaseStream.CopyTo(bw.BaseStream);
+                                }
+                            }
+
+                            bw.BaseStream.Position = 4;
+                            bw.Write((uint)bw.BaseStream.Length - 8);
+
+                            bw.BaseStream.Position = 40;
+                            bw.Write((uint)bw.BaseStream.Length - 44);
+                        }
+
+                        File.WriteAllLines(Path.Combine(file, newcuename), cuesheet);
+                        break;
+                }
+
+                Invoke((MethodInvoker)delegate
+                {
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.YellowGreen;
+                });
+            }
+            );
+
+            MessageBox.Show("Complete");
+        }
+
+        private object int2msf(int leadout, string index)
+        {
+            int gap = msf2int(index);
+
+            leadout += gap;
+
+            int f = leadout / 2352 % 75;
+            int s = leadout / 2352 / 75 % 60;
+            int m = leadout / 2352 / 75 / 60;
+
+            return $"{m:D2}:{s:D2}:{f:D2}";
+        }
+
+        private int msf2int(string index)
+        {
+            string[] msf = index.Split(':');
+
+            return (int.Parse(msf[2]) * 2352) + (int.Parse(msf[1]) * 75 * 2352) + (int.Parse(msf[0]) * 60 * 75 * 2352);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
         }
     }
 }
